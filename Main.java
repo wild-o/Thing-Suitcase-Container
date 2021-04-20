0f68874417d8a80d230b9e5b6f90e9fa7aea1ca7
Albert <alarias1@outlook.com>
2021-04-19 19:00:15 -0500

    first iteration of Thing Suitcase Container project

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        Thing book = new Thing("Happiness in Three Steps", 2);
+        Thing mobile = new Thing("Nokia 3210", 1);
+
+        System.out.println("Book name: " + book.getName());
+        System.out.println("Book weight: " + book.getWeight());
+
+        System.out.println("Book: " + book);
+        System.out.println("Mobile: " + mobile);
+    }
+}
